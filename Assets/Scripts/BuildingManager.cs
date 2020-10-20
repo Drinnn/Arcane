98f9304c6854a264f2c91ebd638a99572aa1c43f
Drinnn <pedro_lontro@hotmail.com>
2020-10-19 22:09:56 -0300

    feat: add building manager initial setup

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+    [SerializeField] private Transform pfWoodHarvester;
+
+    private Camera mainCamera;
+
+    private void Start() {
+        mainCamera = Camera.main;
+    }
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition() {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        return mousePosition;
+    }
+}
