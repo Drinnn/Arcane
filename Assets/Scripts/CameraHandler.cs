49c33053a9b5d12f73698a3b9b7333865cda6422
Drinnn <pedro_lontro@hotmail.com>
2020-10-26 23:50:45 -0300

    feat: add camera movement

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraHandler : MonoBehaviour {
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private float _ortographicSize;
+    private float _targetOrtographicSize;
+
+    private void Start() {
+        _ortographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        _targetOrtographicSize = _ortographicSize;
+    }
+
+    private void Update() {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleMovement() {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(x, y).normalized;
+        float moveSpeed = 30f;
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom() {
+        float zoomAmount = 2f;
+        _targetOrtographicSize -= Input.mouseScrollDelta.y * zoomAmount;
+
+        float minOrtographicSize = 10;
+        float maxOrtographicSize = 30;
+        _targetOrtographicSize = Mathf.Clamp(_targetOrtographicSize, minOrtographicSize, maxOrtographicSize);
+
+        float zoomSpeed = 5f;
+        _ortographicSize = Mathf.Lerp(_ortographicSize, _targetOrtographicSize, Time.deltaTime * zoomSpeed);
+
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = _ortographicSize;
+    }
+}
+
